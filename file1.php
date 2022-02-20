c255634398b4a10e88dedbf2a3c3379eb75d0c46
Pranay Sharma <pranay07417@gmail.com>
2022-02-20 21:56:35 +0530

    first commit of php

---
+++ file1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP Page </h1>
+
+<?php
+$txt = "My first PHP Page";
+echo  "I love my $txt<br>";
+ECHO "HELLO WORLD WITH PHP<br>";
+EcHo "HeLLO<br>";
+
+$color = "red";
+
+echo "My car is ".$color."<br>";
+
+?>
+</body>
+
+
+</html>
+
