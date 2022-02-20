38f21f3f9dda4b8070c8e41dc140ae5be5491923
Pranay Sharma <pranay07417@gmail.com>
2022-02-20 22:18:29 +0530

    about variables

---
+++ Variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt="hello world";
+$x=5;
+$y=10.5;
+
+echo $txt;
+echo '<br>';
+echo $x;
+echo '<br>';
+echo $y;
+echo '<br>';
+$txt2 = 'welcome you all to the php programming language';
+echo 'I '.$txt2.' where we will see server side scripting';
+echo '<br>';
+
+echo $x + $y;
+?>
+</body>
+</html>
