c62c88ec0f8509fce7347a2582db9272ed78b2c2
Pranay Sharma <pranay07417@gmail.com>
2022-02-23 14:24:45 +0530

    about class 1

---
+++ class1.php
@@ -0,0 +1,48 @@
+<?php
+//%d,%h,%i,%ld,%f,%lf,%Lf,%s,%o,%x,%e,%g
+
+printf("pranay");
+echo "<br>";
+printf("%d",78);
+echo "<br>";
+
+echo (printf("%d",78));
+echo "<br>";
+
+echo (printf("%d",7458));
+echo "<br>";
+
+echo(print(printf("%d",printf("pranay sharma"))));
+echo "<br>";
+
+echo(print(printf("%d",print("pranay sharma"))));
+echo "<br>";
+echo(print(printf("%x",printf("pranay sharma"))));
+echo"<br>";
+
+sprintf("%d",78);
+echo"<br>";
+echo(sprintf("%d",78));
+echo "<br>";
+$res = sprintf("%d",78);
+var_dump($res);
+echo "<br>";
+$res = sprintf("%f",78);
+var_dump($res);
+
+echo "<br>";
+
+echo (sprintf(print(printf("%x",printf("yash")))));
+echo "<br>";
+
+$re =0;
+$re = sprintf(printf("%x",printf("PHP examination")));
+echo $re;
+
+echo "<br>";
+
+echo (print('psr 345'));
+
+
+
+?>
