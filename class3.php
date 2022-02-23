0811361d46f53d2a80190c4a8832ae01a8a5f4b1
Pranay Sharma <pranay07417@gmail.com>
2022-02-23 14:29:57 +0530

    about class 1

---
+++ class3.php
@@ -0,0 +1,26 @@
+<?php
+echo addcslashes("hello","o");
+echo "<br>";
+
+echo addslashes("h'ello");
+echo "<br>";
+echo bin2hex("akash");
+echo"<br>";
+
+echo chop("     welcome to the php study");
+echo"<br>";
+
+echo chop("     welcome to the php study","yudts"); // perform as many variations as you can
+echo "<br>";
+echo crc32("");// cyclic redundancy checking
+
+echo"<br>";
+//echo crypt("<");
+
+echo "<br>";
+echo chunk_split("welcome to",2);
+echo "<br>";
+
+
+
+?>
